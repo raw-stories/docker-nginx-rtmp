ca740815d947979af84ecbe7f38649871c58e01a
Paul-Émile MINY <paulemile.miny@gmail.com>
2019-03-24 11:40:34 +0100

    update

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+NGINX_CONFIG_FILE=/opt/nginx/rtmp/main.conf
+
+RTMP_PUSH_URLS=$(echo ${RTMP_PUSH_URLS} | sed "s/,/\n/g")
+
+if [ "x${RTMP_PUSH_URLS}" = "x" ]; then
+    for PUSH_URL in $(echo ${RTMP_PUSH_URLS}); do
+        echo "Pushing stream to ${PUSH_URL}"
+        cat >>${NGINX_CONFIG_FILE} <<!EOF
+        push ${PUSH_URL};
+!EOF
+    done
+fi
+!EOF
+
+echo "Starting server..."
+/opt/nginx/sbin/nginx -g "daemon off;"
