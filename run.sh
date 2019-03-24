eadd79ea37acbff2e467ad2862a54d177e6fe367
Paul-Émile MINY <paulemile.miny@gmail.com>
2019-03-24 12:43:21 +0100

    update

--- run.sh
+++ run.sh
@@ -4,15 +4,14 @@ NGINX_CONFIG_FILE=/opt/nginx/rtmp/main.conf
 
 RTMP_PUSH_URLS=$(echo ${RTMP_PUSH_URLS} | sed "s/,/\n/g")
 
-if [ "x${RTMP_PUSH_URLS}" = "x" ]; then
-    for PUSH_URL in $(echo ${RTMP_PUSH_URLS}); do
-        echo "Pushing stream to ${PUSH_URL}"
-        cat >>${NGINX_CONFIG_FILE} <<!EOF
-        push ${PUSH_URL};
+for PUSH_URL in $(echo ${RTMP_PUSH_URLS}); do
+    echo "Pushing stream to ${PUSH_URL}"
+    cat >>${NGINX_CONFIG_FILE} <<!EOF
+    push ${PUSH_URL};
 !EOF
-    done
-    cat ${NGINX_CONFIG_FILE}
-fi
+done
+echo "Config :"
+cat ${NGINX_CONFIG_FILE}
 
 echo "Starting server..."
 /opt/nginx/sbin/nginx
